02f2fed6d047d6201985a7ea4819968f6f1b3ed4
Tomer Shoham <tshoham@fireblocks.com>
2025-05-07 13:48:31 +0300

    feat: txSignReq, configChangeSignReq

--- src/router.ts
+++ src/router.ts
@@ -1,4 +1,5 @@
 import { Router, Request, Response } from "express";
+import { configChangeSignReq, txSignReq } from "./controller";
 
 const router: Router = Router();
 
@@ -6,5 +7,9 @@ router.get("/health", (_req: Request, res: Response) => {
   console.log("alive");
   res.status(200).send("Alive");
 });
+
+router
+  .post("/v2/tx_sign_request", txSignReq)
+  .post("/v2/config_change_sign_request", configChangeSignReq);
 
 export default router;
